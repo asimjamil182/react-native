0ffe354809a2da27bd12585740a0a8e27eb937c7
asimjamil182 <asimjamil182@gmail.com>
2024-06-21 20:57:59 +0400

    updated

--- app/index.js
+++ app/index.js
@@ -1,8 +1,14 @@
-const { Text } = require("react-native")
+import { StatusBar } from "expo-status-bar";
+
+const { Text, View } = require("react-native")
 
 const Index = () => {
     return (
-        <Text>Index Page</Text>
+
+        <View className="flex items-center justify-center">
+            <Text className="text-red-400">Open up App.js to start working on your app!</Text>
+            <StatusBar  backgroundColor="#333" />
+        </View>
     )
 }
 
